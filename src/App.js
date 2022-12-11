704b2a22ccb1b87b390d20b01e349c7286e66a5e
Subham Sahu <about.subhamsahu@gmail.com>
2022-12-12 00:27:10 +0530

    11th Dec 2022 v83-setting up homepage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React from "react";
-import Directory from "./components/directory/Directory";
+import Home from "./components/routes/home/Home";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route exact path="/home" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
