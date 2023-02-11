2eb8c6f1d5f28d07437098b97d986cd1cdbb1acc
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-11 14:52:10 +0530

    ..

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -3,6 +3,7 @@ import "./button.styles.scss";
 export const BUTTON_TYPE_CLASSES = {
   google: "google-sign-in",
   inverted: "inverted",
+  addToCartButton: "addToCartButton",
 };
 
 const Button = ({ children, buttonType, ...otherProps }) => {
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,17 +1,27 @@
-import './product-card.styles.scss';
-
-import Button from '../button/button.component';
+import "./product-card.styles.scss";
+import Button from "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+const addProductToCart = () => addItemToCart(product)
+
   return (
-    <div className='product-card-container'>
+    <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
-      <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
-      <Button buttonType='inverted'>Add to card</Button>
+      <Button
+        buttonType="addToCartButton"
+        onClick={addProductToCart}
+      >
+        Add to card
+      </Button>
     </div>
   );
 };
