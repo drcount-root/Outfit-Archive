93064d3c6b906be514c3c73788aab5c69ea52c69
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-12 18:22:29 +0530

    v136 : style

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import ProductCard from "../product-card/product-card.component";
 
 import "./category-preview.styles.scss";
@@ -5,7 +7,9 @@ import "./category-preview.styles.scss";
 const CategoryPreview = ({ title, products }) => (
   <div className="category-preview-container">
     <h2>
-      <span className="title">{title.toUpperCase()}</span>
+      <Link className="title" to={title}>
+        {title.toUpperCase()}
+      </Link>
     </h2>
     <div className="preview">
       {products
