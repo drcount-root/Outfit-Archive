33b1d507f5fcd1353b5d006c4b11fe745dce9a57
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-13 20:15:24 +0530

    grid fix

--- src/components/category-item/directory-item.component.jsx
+++ src/components/category-item/directory-item.component.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 import "./directory-item.styles.scss";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigateHandler}>
       <div
         className="background-image"
         style={{
