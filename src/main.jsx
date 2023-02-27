14adf6e3d6d68e5bd332853e08e8ff56c507f348
Subham Sahu <about.subhamsahu@gmail.com>
2023-02-27 22:33:12 +0530

    154: Setting up store done

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,26 @@ import "./index.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+// redux store
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
